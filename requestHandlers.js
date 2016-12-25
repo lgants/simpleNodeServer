deea95d76058befab05903fd4609dc25025fd4dc
lgants <lgants@gmail.com>
2016-12-24 23:28:27 -0600

    nonblocking implementation

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,19 +1,31 @@
-function start(){
+var exec = require("child_process").exec;
+
+function start(response){
   console.log("Request handler 'start' was called.");
 
-  //blocking operation in start function 
-  function sleep(milliSeconds){
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSeconds);
-  }
+  exec("ls -lah", function(error, stdout, stderr){
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(stdout);
+    response.end();
+  });
 
-  sleep(10000);
-  return "Hello Start"
+  //blocking operation in start function
+  // function sleep(milliSeconds){
+  //   var startTime = new Date().getTime();
+  //   while (new Date().getTime() < startTime + milliSeconds);
+  // }
+  //
+  // sleep(10000);
+  // return "Hello Start"
+
+  // return content;
 }
 
-function upload(){
+function upload(response){
   console.log("Request handler 'upload' was called.");
-  return "Hello Upload";
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
