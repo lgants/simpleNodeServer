19729dc1cb34ec4d512fd8563b1b892b628661e4
lgants <lgants@gmail.com>
2016-12-24 22:25:50 -0600

    added requestHandler module

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers")
 
-//injects route function of router into server 
-server.start(router.route);
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+//injects route function of router into server
+server.start(router.route, handle);
