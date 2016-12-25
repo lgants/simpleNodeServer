6598c9f204d6d533b0f30a3e0f6bad776d0c5e9f
lgants <lgants@gmail.com>
2016-12-24 22:41:11 -0600

    blocking demo completed

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,19 @@
 function start(){
   console.log("Request handler 'start' was called.");
+
+  //blocking operation in start function 
+  function sleep(milliSeconds){
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+  return "Hello Start"
 }
 
 function upload(){
   console.log("Request handler 'upload' was called.");
+  return "Hello Upload";
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -6,10 +6,11 @@ function start(route, handle) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
+    // route(handle, pathname);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    var content = route(handle, pathname);
+    response.write(content);
     response.end();
   }
 
